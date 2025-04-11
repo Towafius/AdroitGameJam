a7e42757404aedd8308afc0fced4fa38e393c6d6
Ian Shaffer <towafius@gmail.com>
2025-04-11 18:40:44 -0500

    stuff

--- adriotgamejam2025/Scenes/Menu/main_menu.gd
+++ adriotgamejam2025/Scenes/Menu/main_menu.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://Scenes/Levels/TestWorld.tscn")
 
 func _on_settings_button_pressed() -> void:
 	pass # Replace with function body.
