7849ab95172384df6411e332b0f3d6944c227041
Towafius <towafius@gmail.com>
2025-04-11 22:12:55 -0500

    Player Animations

--- adriotgamejam2025/Scenes/Player/player.gd
+++ adriotgamejam2025/Scenes/Player/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const SPEED = 100
+
+var last_direction = Vector2(0,1)
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -15,4 +18,36 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	handle_animation(direction)
+
+	if(direction):
+		last_direction = direction
+
 	move_and_slide()
+
+
+func handle_animation(direction:Vector2):
+	if(direction.is_equal_approx(Vector2.ZERO)):
+		if last_direction.x:
+			if last_direction.x > 0:
+				animation_player.play("idle_right")
+			else:
+				animation_player.play("idle_left")
+		elif last_direction.y:
+			if last_direction.y > 0:
+				animation_player.play("idle_down")
+			else:
+				animation_player.play("idle_up")
+		else:
+			animation_player.play("idle_down")
+	else:
+		if direction.x:
+			if direction.x > 0:
+				animation_player.play("walk_right")
+			else:
+				animation_player.play("walk_left")
+		else:
+			if direction.y > 0:
+				animation_player.play("walk_down")
+			else:
+				animation_player.play("walk_up")
--- adriotgamejam2025/Scenes/Player/weapon.gd
+++ adriotgamejam2025/Scenes/Player/weapon.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+@onready var weapon_animator: AnimationPlayer = $WeaponAnimator
+@onready var attack_timer: Timer = $Attack_Timer
+
+enum Attacks{
+	swing,
+	swing2
+}
+
+var last_attack:int=-1
+var can_attack:bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +19,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("attack") && can_attack:
+		if(last_attack != Attacks.swing):
+			last_attack = Attacks.swing
+			weapon_animator.play("Swing")
+		else:
+			last_attack = Attacks.swing2
+			weapon_animator.play("Swing_2")
+		attack_timer.start()
+		can_attack = false
+
+
+func _on_attack_timer_timeout() -> void:
+	can_attack = true
