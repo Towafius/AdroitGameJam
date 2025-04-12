3e411f85678eba4347c10153ed51214ba4969e59
Towafius <towafius@gmail.com>
2025-04-11 21:00:34 -0500

    Added Assets

--- adriotgamejam2025/Scenes/Player/player.gd
+++ adriotgamejam2025/Scenes/Player/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
+	var direction := Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.y = direction.y * SPEED
